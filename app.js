69e6b061c86bfda9e2c92162fa116b5290a7735c
tpulli <ejiqpa@gmail.com>
2014-10-12 23:24:17 +0300

    Basic mongo

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+var app = express();
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+mongoose.connect("mongodb://localhost/clickganic");
+
+app.use(bodyParser());
+
+var catSchema = mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
+app.get("/cats", function(req, res) {
+  // res.render('index', object);
+  Cat.find(function(err, cats) {
+    res.render("cats", {cats: cats});
+  });
+});
+
+app.post("/create_cat", function(req, res) {
+  var kitty = new Cat({name: req.body.name});
+  kitty.save(function(err) {
+    console.log("meow");
+    res.redirect("cats");
+  });
+});
+
+app.get("/cats/new", function(req, res) {
+  res.render("create_cat");
+});
+
+app.get("/cats/:id", function(req, res) {
+  Cat.findById(req.params.id, function(err, cat) {
+    res.render("cat", {cat: cat});
+  });
+  // console.log(req.params.id);
+});
+
+app.listen(2000, function() {
+  console.log("Go!");
+});
